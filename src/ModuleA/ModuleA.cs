ecb052f0ece256df30f5754fa91ce25af4e7275c
gbull122 <gbull122@gmail.com>
2019-03-12 21:44:29 +0000

    Added ModuleA as seperate library Removed all project wide command stuff and context

---
+++ src/ModuleA/ModuleA.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ModuleA
+{
+    public class ModuleA : IModule
+    {
+
+        public ModuleA(IRegionManager regManager)
+        {
+            regManager.RegisterViewWithRegion("RibbonRegion", typeof(RibbonView));
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+         
+        }
+    }
+}
